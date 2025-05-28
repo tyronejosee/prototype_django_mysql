22d54a17183aed43253581ee1fdf410540963d56
tyronejose <alt.tyronejose@gmail.com>
2025-05-28 18:41:26 -0400

    feat: add initial project structure with docker, django settings, and requirements

---
+++ config/settings/base.py
@@ -0,0 +1,183 @@
+"""Settings for config project (Base)."""
+
+import os
+from pathlib import Path
+import sys
+from datetime import timedelta
+import environ
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+env = environ.Env()
+environ.Env.read_env(".env")
+
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG = env("DEBUG", default=False)
+
+BASE_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+PROJECT_APPS = [
+    "apps.utils",
+]
+
+THIRD_APPS = [
+    # ...
+]
+
+INSTALLED_APPS = BASE_APPS + PROJECT_APPS + THIRD_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
+
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+INTERNAL_IPS = env.list("INTERNAL_IPS")
+
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS")
+
+if "test" in sys.argv:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": env("SQL_DATABASE"),
+            "USER": env("SQL_USER"),
+            "PASSWORD": env("SQL_PASSWORD"),
+            "HOST": "db",
+            "PORT": "3306",
+        }
+    }
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_CONTENT_LANGUAGE": "en",
+    # "DEFAULT_PAGINATION_CLASS": "apps.utilities.pagination.LimitSetPagination",
+    "DEFAULT_FILTER_BACKENDS": [
+        "rest_framework.filters.SearchFilter",
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "3/second",
+        "user": "60/minute",
+        "daily": "1000/day",
+    },
+    "NUM_PROXIES": None,
+    "PAGE_SIZE": 25,
+    "SEARCH_PARAM": "q",
+    "ORDERING_PARAM": "order",
+}
+
+# AUTH_USER_MODEL = "users.User" # Update
+
+CORS_ALLOW_HEADERS = [
+    "Authorization",
+    "Content-Type",
+    "accept",
+    "accept-encoding",
+    "content-disposition",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Example API",
+    "DESCRIPTION": "Example decription.",
+    "VERSION": "v1",
+    "LICENSE": {
+        "name": env("LICENCE_NAME"),
+        "url": env("LICENCE_URL"),
+    },
+    "CONTACT": {
+        "name": env("CONTACT_NAME"),
+        "url": env("CONTACT_URL"),
+    },
+    "SCHEMA_PATH_PREFIX_TRIM": True,
+    "SERVE_INCLUDE_SCHEMA": False,
+}
